55a4a5cc6b73454f42d0f39656428254effaadaf
lucasmetron <lucasmetron@gmail.com>
2021-06-25 16:28:21 -0300

    inicio do react com npx

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite o arquivo <code>src/App.js</code> e salve para recarregar.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Saiba mais :D
         </a>
       </header>
     </div>
