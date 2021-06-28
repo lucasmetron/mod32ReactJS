0ae0338c34399f4ecfd8d3c38bac5db93f0657fd
lucasmetron <lucasmetron@gmail.com>
2021-06-28 18:31:51 -0300

    estado

--- meu-app/src/Counter.js
+++ meu-app/src/Counter.js
@@ -8,13 +8,16 @@ class Counter extends React.Component {
 
         this.state = { count: this.props.count } //só aqui podemos utilizar o this.state
         this.add = this.add.bind(this)
+        console.log(this)
     }
 
 
     add() {
         // this.setState({ count: this.state.count + 1 }) //sempre que formos alterar um estado nao podemos alterar diretamento com this.state()
         this.setState((state) => { //Melhor forma é colocando função, pq o setState cria uma fila de execução e garante que todos estado seram ataulizados.
+
             return { count: state.count + 1 }
+
         })
     }
 
