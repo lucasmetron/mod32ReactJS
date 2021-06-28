4e90edb319ed5df1aa6809eae37b5111dda1e299
lucasmetron <lucasmetron@gmail.com>
2021-06-28 18:17:33 -0300

    estado

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Counter from './Counter.js';
 
 function App() {
   return (
-    <Header name="IBTI" links={["Sobre", "Comprar", "Contato"]}></Header>
+    <div>
+      <Header name="José" links={["Sobre", "Comprar", "Contato"]}></Header>
+      <Counter count={0}></Counter>
+    </div>
   );
+
+
 }
 
 export default App;
---
+++ meu-app/src/Counter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+class Counter extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { count: this.props.count } //só aqui podemos utilizar o this.state
+        this.add = this.add.bind(this)
+    }
+
+
+    add() {
+        // this.setState({ count: this.state.count + 1 }) //sempre que formos alterar um estado nao podemos alterar diretamento com this.state()
+        this.setState((state) => { //Melhor forma é colocando função, pq o setState cria uma fila de execução e garante que todos estado seram ataulizados.
+            return { count: state.count + 1 }
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Counter: {this.state.count}</h1>
+                <button onClick={this.add}>add</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
