93e99ebabf55b25c8aed491e1ac3553c3c8ab6f4
lucasmetron <lucasmetron@gmail.com>
2021-06-29 10:24:49 -0300

    hooks

--- meu-app/src/Counter.js
+++ meu-app/src/Counter.js
@@ -1,45 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
-class Counter extends React.Component {
+function Counter(props) {
 
-    constructor(props) {
-        super(props);
+    const [count, setCount] = useState(props.count) //a segunda opção da array, sempre deve ser set+o nome da primeira posição da array, para manter o padrão. Ex: [nome, setNome]
+    const [meuEstado, setMeuEstado] = useState('qualquer coisa');
 
-        this.state = { count: 0 } //só aqui podemos utilizar o this.state
-        this.add = this.add.bind(this)
+    function add() {
+        setCount(count + 1);
+        console.log('clicado')
+        setMeuEstado(meuEstado + ' teste')
+        console.log(meuEstado);
     }
 
-
-    add() {
-        // this.setState({ count: this.state.count + 1 }) //sempre que formos alterar um estado nao podemos alterar diretamento com this.state()
-        this.setState((state) => { //Melhor forma é colocando função, pq o setState cria uma fila de execução e garante que todos estado seram ataulizados.
-
-            return { count: state.count + 1 }
-
-        }, () => {
-            localStorage.setItem('state', JSON.stringify(this.state));
-        })
-    }
-
-
-    componentDidMount() {
-        this.setState(JSON.parse(localStorage.getItem('state')))
-    }
-
-    shouldComponentUpdate() {
-        return true;
-    }
-
-
-    render() {
-        return (
-            <div>
-                <h1>Counter: {this.state.count}</h1>
-                <button onClick={this.add}>add</button>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={add}>add</button>
+        </div>
+    );
 }
+
+// class Counter extends React.Component {
+
+//     constructor(props) {
+//         super(props);
+
+//         this.state = { count: 0 } //só aqui podemos utilizar o this.state
+//         this.add = this.add.bind(this)
+//     }
+
+
+//     add() {
+//         // this.setState({ count: this.state.count + 1 }) //sempre que formos alterar um estado nao podemos alterar diretamento com this.state()
+//         this.setState((state) => { //Melhor forma é colocando função, pq o setState cria uma fila de execução e garante que todos estado seram ataulizados.
+
+//             return { count: state.count + 1 }
+
+//         }, () => {
+//             localStorage.setItem('state', JSON.stringify(this.state));
+//         })
+//     }
+
+
+//     componentDidMount() {
+//         this.setState(JSON.parse(localStorage.getItem('state')))
+//     }
+
+//     shouldComponentUpdate() {
+//         return true;
+//     }
+
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Counter: {this.state.count}</h1>
+//                 <button onClick={this.add}>add</button>
+//             </div>
+//         )
+//     }
+// }
 
 export default Counter;
